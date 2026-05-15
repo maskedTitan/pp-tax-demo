1f5029c0989663dc07771eea30095646a4a0a2ff
Harsha Marri <harshamarri@Mac.ht.home>
2026-05-15 02:05:36 -0500

    Security fix: Remove hardcoded Braintree credentials

--- src/lib/server/braintree.js
+++ src/lib/server/braintree.js
@@ -3,7 +3,7 @@ import { env } from '$env/dynamic/private';
 
 export const gateway = new braintree.BraintreeGateway({
     environment: braintree.Environment.Sandbox,
-    merchantId: env.BRAINTREE_MERCHANT_ID || 'b6bnqwx858gzdsdc',
-    publicKey: env.BRAINTREE_PUBLIC_KEY || '7mhg2kbhfkxqrhdb',
-    privateKey: env.BRAINTREE_PRIVATE_KEY || '843cf1b5cfc9f6592a30c882795fa18a'
+    merchantId: env.BRAINTREE_MERCHANT_ID,
+    publicKey: env.BRAINTREE_PUBLIC_KEY,
+    privateKey: env.BRAINTREE_PRIVATE_KEY
 });
